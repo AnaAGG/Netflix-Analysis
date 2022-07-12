7e321ff420dedb0fa50f7c8db2664dc985e59000
AnaGG <a.garciagar5@gmail.com>
2022-07-12 23:10:49 +0200

    actualizando

--- src/support.py
+++ src/support.py
@@ -1,42 +1,52 @@
 import pandas as pd
 
 class Cleaning():
-    def __init__(self, df):
+    def __init__(self, df, premiere_column, cast_director_column):
         self.df = df
-    
-    def get_month(self, column_name):
-
-        self.column_name = column_name
+        self.premiere_column = premiere_column
+        self.cast_director_column = cast_director_column
 
 
-        if type(column_name) == str:
-            return column_name.split()[0] 
+    def get_month(self):
+
+        if type(self.premiere_column) == str:
+            return self.premiere_column.split()[0] 
         else: 
-            return column_name
+            return self.premiere_column
+        
+        
     
-    
-    def get_year(self, column_name):
+    def get_year(self):
 
-        self.column_name = column_name
-
-        if type(column_name) == str:
+        if self.df[self.premiere_column].dtypes== "O":
             try: 
-                return column_name.split(",")[1]
+                
+                return self.df[self.premiere_column].str.split(",", n = 1).str.get(1)
             except: 
-                return column_name.split(".")[1]
+                return self.df[self.premiere_column].str.split(".", n = 1).str.get(1)
         else: 
             pass
 
     
-    def split_column(self, column_name):
+    def split_column(self):
 
-        self.column_name = column_name
-
-        if type(column_name) == str:
-            return column_name.split(',')
+        if type(self.cast_director_column ) == str:
+            return self.cast_director_column .split(',')
         
         else: 
-            return column_name
+            return self.cast_director_column 
+
+
+    def clean(self): 
+
+        self.df["month"] = self.df.apply(lambda x: self.get_month(), axis = 1)
+        results = self.df.apply(lambda x: self.get_year(), axis=1)
+        self.df[self.cast_director_column] = self.df.apply(lambda x:self.split_column(), axis=1)
+
+        print(results)
+        self.df.to_csv("prbamdo_clase.csv")
+
+        return self.df
 
 
 
